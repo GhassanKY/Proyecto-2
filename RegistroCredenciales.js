b6903455bfddf098b992cdce9069f2747fc60ecd
GhassanKY <elkhouryghassan712@gmail.com>
2022-06-23 13:55:04 +0200

    Boton y Comenzamos a generar funciones en JS

---
+++ RegistroCredenciales.js
@@ -0,0 +1,18 @@
+var boton = document.querySelector("BotonRegistrar")
+
+var almacenamientoOBJ = []
+
+function Boton(event){
+
+var nombre = document.getElementById("Nombre").value
+var contraseña = document.getElementById("Contraseña").value
+var email =  document.getElementById("Email").value
+
+}
+
+function objetosMaker (){
+    nombre = nombre
+    contraseña = contraseña
+    email = email
+}
+
